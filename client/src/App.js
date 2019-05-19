a1e3307a5caba65a2ef218bddc33744cb654f944
Sebastian Kozub <sebastiankozub@o2.pl>
2019-05-20 00:59:28 +0200

    font end plays

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import getWeb3 from "./utils/getWeb3";
 import "./App.css"; 
  
 class App extends Component { 
-  state = { storageValue: 0, web3: null, accounts: null, contract: null, responseArray: null }; 
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, responseArray: [0] }; 
 
   componentDidMount = async () => { 
     try {
@@ -25,7 +25,7 @@ class App extends Component {
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance, responseArray: [0] }, this.runExample);
+      this.setState({ web3, accounts, contract: instance}, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -38,23 +38,24 @@ class App extends Component {
   runExample = async () => {
     const { accounts, contract } = this.state;
 
+    // count gas price and amount - now hard coded
     const response1 = await contract.methods.getCertificatesCount().call();
-    const response2 = await contract.methods.createNewCertificate("11111111","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg",["gdsfgdsfg"],["gdsfgdsfg","11111111111"]).send({from:accounts[0]});
-    const response3 = await contract.methods.createNewCertificate("22222222","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg",["gdsfgdsfg"],["gdsf3333422222","2222222"]).send({from:accounts[0]});
-    const response4 = await contract.methods.createNewCertificate("33333333","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg",["gdsfgdsfg"],["gdsf3333","23333333"]).send({from:accounts[0]});
+    const response2 = await contract.methods.createNewCertificate("11111111","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg",["gdsfgdsfg"],["gdsfgdsfg","11111111111"]).send({from:accounts[0],gasPrice : this.state.web3.utils.toWei("5", "gwei"), gas : this.state.web3.utils.toWei("0.0000000000005", "ether")});
+    const response3 = await contract.methods.createNewCertificate("22222222","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg",["gdsfgdsfg"],["gdsf3333422222","2222222"]).send({from:accounts[0], gasPrice: this.state.web3.utils.toWei("5", "gwei"), gas : this.state.web3.utils.toWei("0.0000000000005", "ether")});
+    const response4 = await contract.methods.createNewCertificate("33333333","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg","gdsfgdsfg",["gdsfgdsfg"],["gdsf3333","23333333"]).send({from:accounts[0], gasPrice: this.state.web3.utils.toWei("5", "gwei"),  gas : this.state.web3.utils.toWei("0.0000000000005", "ether")});
     const response5 = await contract.methods.getCertificatesCount().call();
 
-    let txHash1 = response1.transactionHash1;
-    let txHash2 = response2.transactionHash2;
-    let txHash3 = response3.transactionHash3;
-    let txHash4 = response4.transactionHash4;
-    let txHash5 = response5.transactionHash5;
+    // let txHash1 = response1.transactionHash1;
+    // let txHash2 = response2.transactionHash2;
+    // let txHash3 = response3.transactionHash3;
+    // let txHash4 = response4.transactionHash4;
+    // let txHash5 = response5.transactionHash5;
 
-    console.log(response1);
-    console.log(response2);
-    console.log(response3);
-    console.log(response4);
-    console.log(response5);
+    // console.log(response1);
+    // console.log(response2);
+    // console.log(response3);
+    // console.log(response4);
+    // console.log(response5);
 
     var tempArray = [response1, response2.cumulativeGasUsed, response3.cumulativeGasUsed, response4.cumulativeGasUsed, response5];
     this.setState({ responseArray: tempArray});
@@ -66,15 +67,11 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
+        <h1>Good to Go! Go! Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
         <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
+          Few methods to test cooperation with MetaMask, Ganache, lite-server and few other tools...
         </p>
         <div>getCertificatesCount response1 value is: {this.state.responseArray[0]}</div>
         <div>createNewCertificate response2 value is: {this.state.responseArray[1]}</div>
@@ -83,7 +80,8 @@ class App extends Component {
         <div>getCertificatesCount response5 value is: {this.state.responseArray[4]}</div>
       </div>
     );
-  }
+  };
 }
 
 export default App;
+
